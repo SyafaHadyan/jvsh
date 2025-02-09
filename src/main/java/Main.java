dffa8bb45c7722c3009a98403aa9f35093ca0fea
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2025-02-10 06:18:48 +0700

    Echo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,20 +2,39 @@ import java.util.Scanner;
 
 public class Main
 {
+    static Scanner input = new Scanner(System.in);
+    private static void command(String userInput)
+    {
+        if (userInput.equals("exit 0"))
+        {
+            exit(userInput);
+        }
+        else if (userInput.contains("echo"))
+        {
+            echo(userInput.replaceAll("echo ", ""));
+        }
+        else
+        {
+            System.out.println(userInput + ": command not found");
+        }
+    }
+    private static void exit(String userInput)
+    {
+        input.close();
+        System.exit(Integer.parseInt(userInput.split("\\s+")[1]));
+    }
+    private static void echo(String userInput)
+    {
+        System.out.println(userInput);
+    }
     public static void main(String[] args) throws Exception
     {
-        Scanner input = new Scanner(System.in);
         String userInput = "";
         while (true)
         {
             System.out.print("$ ");
             userInput = input.nextLine();
-            if (userInput.equals("exit 0"))
-            {
-                break;
-            }
-            System.out.println(userInput + ": command not found");
+            command(userInput);
         }
-        input.close();
     }
 }
