af856df4d61c5cc130ad9fd5b0f4ce6a7bbfcaaf
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2025-02-10 06:30:55 +0700

    Type

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,27 +5,51 @@ public class Main
     static Scanner input = new Scanner(System.in);
     private static void command(String userInput)
     {
-        if (userInput.equals("exit 0"))
+        String[] input = userInput.split("\\s+");
+        if (input[0].equals("exit"))
         {
-            exit(userInput);
+            exit(Integer.parseInt(input[1]));
         }
-        else if (userInput.contains("echo"))
+        else if (input[0].equals("echo"))
         {
-            echo(userInput.replaceAll("echo ", ""));
+            echo(userInput.replaceFirst("echo ", ""));
+        }
+        else if (input[0].equals("type"))
+        {
+            type(userInput.replaceFirst("type ", ""));
         }
         else
         {
             System.out.println(userInput + ": command not found");
         }
     }
-    private static void exit(String userInput)
+    private static void exit(int statusCode)
     {
         input.close();
-        System.exit(Integer.parseInt(userInput.split("\\s+")[1]));
+        System.exit(statusCode);
     }
     private static void echo(String userInput)
     {
         System.out.println(userInput);
+    }
+    private static void type(String userInput)
+    {
+        if (userInput.equals("echo"))
+        {
+            System.out.println("echo is a shell builtin");
+        }
+        else if (userInput.equals("exit"))
+        {
+            System.out.println("exit is a shell builtin");
+        }
+        else if (userInput.equals("type"))
+        {
+            System.out.println("type is a shell builtin");
+        }
+        else
+        {
+            System.out.println(userInput + ": not found");
+        }
     }
     public static void main(String[] args) throws Exception
     {
